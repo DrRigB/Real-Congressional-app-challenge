eb0b043f3b497addaac480f451e201de88171240
DrRigB <rigdon.windsor@gmail.com>
2024-10-21 09:34:35 -0400

    Added character placeholder and brants movemnent code

--- coral.gd
+++ coral.gd
@@ -1,11 +1,11 @@
-extends RigidBody2D
+extends RigidBody2D  # Ensure your script extends RigidBody2D
 
-@onready var player: CharacterBody2D = $"../../player"
-
-func _ready():
-	# Connect the body_entered signal to the RigidBody2D
-	self.connect("body_entered", Callable(self, "_on_body_entered"))
-
-func _on_body_entered(body):
-	if body == player:  # Check if the colliding body is the player
-		queue_free()  # Remove the creature from the scene upon collision
+#@onready var player: CharacterBody2D = get_parent().get_node("player")  # Adjust the path accordingly
+#@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+#
+#func _ready():
+	#collision_shape_2d.connect("body_entered", self, "_on_body_entered")
+#
+#func _on_body_entered(body):
+	#if body == player:
+		#queue_free()
